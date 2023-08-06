21387a8adbcfc3a4612a4564c05071ca12ad105c
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-08-06 10:00:34 +0530

    simplyfig te files

--- models/product.js
+++ models/product.js
@@ -2,19 +2,23 @@ const dirPath = require("../util/path");
 const path = require("path");
 const fs = require("fs");
 
+const p = path.join(dirPath, "data", "product.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    const p = path.join(dirPath, "data", "product.json");
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -23,15 +27,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(dirPath, "data", "product.json");
-
-   return fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        // return [];
-        cb([])
-      }
-    //   return JSON.parse(fileContent);
-    cb(JSON.parse(fileContent))
-    });
+    getProductsFromFile(cb);
   }
 };
